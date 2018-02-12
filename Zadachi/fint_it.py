14b60a830e1d560750909785226669ea6e669a53
salgal1975 <salgal@list.ru>
2018-02-13 00:24:40 +0500

    stop

--- Zadachi/fint_it.py
+++ Zadachi/fint_it.py
@@ -6,7 +6,22 @@
 Всегда будет только одно целое число, которое появляется нечетным числом раз.
 (find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]), 5)
 '''
-# git push
+
 
 def find_it(seq):
-    count = 0
+    res = []
+    a = []
+    for i in range(len(seq)):
+        res.append([seq.count(seq[i]), seq[i]])
+    for i in res:
+        if i[0] % 2 != 0:
+            a.append(i[1])
+    if len(a) != 1:
+        a = a[0]
+        # print(a)
+        return a
+    # print(type(a), a)
+
+
+seq = [20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]
+find_it(seq)
